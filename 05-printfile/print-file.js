8d0622f9c474f330e13b6b8c5bf190c5030ec692
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-15 11:47:46 +0530

    read file async

--- 05-printfile/print-file.js
+++ 05-printfile/print-file.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
-
-const fileContents = fs.readFileSync('sample.txt', { encoding : 'utf8'})
-console.log(fileContents)
+try {
+    const fileContents = fs.readFileSync('sample1.txt', { encoding : 'utf8'})
+    console.log(fileContents);
+} catch (err) {
+    console.log('something went wrong')
+    console.log(err)
+}
