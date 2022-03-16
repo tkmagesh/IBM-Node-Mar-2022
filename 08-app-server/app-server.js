e1d9210bc78484e6749ffbdb13af65d15494f8cd
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-16 11:25:23 +0530

    app server assignment

---
+++ 08-app-server/app-server.js
@@ -0,0 +1,7 @@
+/* 
+    server should listen on 9090
+    request => http://localhost:9090/calculator?op=add&n1=100&n2=200
+        response => 300
+    if the request is not for '/calculator'
+        then send 404
+*/
