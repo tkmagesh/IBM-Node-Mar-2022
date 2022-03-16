99607ce8fd3bb804ca05619f1bbdf0aaa459a7dd
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-16 11:59:47 +0530

    app server (for get requests)

--- 08-app-server/app-server.js
+++ 08-app-server/app-server.js
@@ -5,3 +5,39 @@
     if the request is not for '/calculator'
         then send 404
 */
+
+const http = require('http'),
+    url = require('url'),
+    calculator = require('./calculator');
+
+const server = http.createServer((req, res) => {
+    const urlObj = new url.URL(req.url, 'http://localhost:8080');
+
+    if (urlObj.pathname === '/calculator'){
+        //extracting querystring values
+        const querystringObj = {}
+        for (let [key, value] of urlObj.searchParams){
+            querystringObj[key] = value
+        };
+        const op = querystringObj.op,
+            n1 = parseInt(querystringObj.n1),
+            n2 = parseInt(querystringObj.n2);
+        if (!calculator.hasOwnProperty(op)){
+            res.statusCode = 400;
+            res.end()
+            return
+        }
+        const result = calculator[op](n1, n2)
+        res.write(result.toString());
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+});
+
+server.listen(9090);
+
+server.on('listening', () => {
+    console.log('app-server listening on 9090')
+})
---
+++ 08-app-server/calculator.js
@@ -0,0 +1,18 @@
+
+var calculator = {
+    add(x,y){
+        return x + y
+    },
+    subtract(x,y){
+        return x - y
+    },
+    multiply(x,y){
+        return x * y
+    },
+    divide(x,y){
+        return x / y
+    }
+}
+
+//console.log('module.exports = ', module.exports)
+module.exports = calculator;
